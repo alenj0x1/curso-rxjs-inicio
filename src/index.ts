9189bea8ad8f66f8ef5be28e5da038431352f9d2
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-06-08 12:53:45 -0500

    subscription and unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,38 @@
 import { Observable, Observer } from "rxjs";
 
-const observer: Observer<string> = {
+const observer: Observer<number> = {
   next: (data) => {
-    console.log(data);
+    console.log("next: ", data);
   },
   error: (err) => {
-    console.error(err);
+    console.error("error", err);
   },
   complete: () => {
     console.info("completed");
   },
 };
+
+const interval$ = new Observable<number>((subscriber) => {
+  let count = 0;
+
+  const interval = setInterval(() => {
+    count++;
+    console.log(count);
+    subscriber.next(count);
+  }, 1000);
+
+  return () => {
+    clearInterval(interval);
+    console.log("interval destroyed");
+  };
+});
+
+const sub1 = interval$.subscribe();
+const sub2 = interval$.subscribe();
+const sub3 = interval$.subscribe();
+
+setTimeout(() => {
+  sub1.unsubscribe();
+  sub2.unsubscribe();
+  sub3.unsubscribe();
+}, 3000);
