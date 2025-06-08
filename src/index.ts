dee18c7a243946392ee291b9067eebe41e731d86
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-06-08 12:20:12 -0500

    first observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import { Observable } from "rxjs";
 
+const obs$ = new Observable<string>((subs) => {
+  subs.next("taka");
+  subs.next("taka 2");
 
-console.log('Hola Mundo!');
+  subs.complete();
 
+  subs.next("taka");
+});
 
-
-
-
-
-
+obs$.subscribe((resp) => console.log(resp));
