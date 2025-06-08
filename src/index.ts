95fa47afc76d00a06713e08c284abd08da0ac531
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-06-08 12:32:47 -0500

    observer and subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-import { Observable } from "rxjs";
+import { Observable, Observer } from "rxjs";
+
+const observer: Observer<string> = {
+  next: (data) => {
+    console.log(data);
+  },
+  error: (err) => {
+    console.error(err);
+  },
+  complete: () => {
+    console.info("completed");
+  },
+};
 
 const obs$ = new Observable<string>((subs) => {
   subs.next("taka");
@@ -9,4 +21,4 @@ const obs$ = new Observable<string>((subs) => {
   subs.next("taka");
 });
 
-obs$.subscribe((resp) => console.log(resp));
+obs$.subscribe(observer);
